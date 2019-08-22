9e5c596a88bdef0d76cdf331e5c272f9fe23426a
nsraktasnsr <51508732+nsraktasnsr@users.noreply.github.com>
2019-08-22 20:30:49 +0200

    Update 10_hafta_odev_amiral_batti_oyunu.py

--- 10_hafta_odev_amiral_batti_oyunu.py
+++ 10_hafta_odev_amiral_batti_oyunu.py
@@ -2,6 +2,7 @@ import random as r
 import time
 print('b.')
 print('>>>>>>>>>>>>>>>>>>>>...Amiral batti oyununa HOSGELDINIZ...<<<<<<<<<<<<<<<<<<<<\n'
+      '>>>>>>>>>>>>>>>>>>>>>>>>>>>>.......1.0.......<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<\n'
       '2 tane 4 birim \t( * * * * )\n2 tane 3 birim\t(  * * *  )\n'
       '2 tane 2 birim \t(   * *   )\n2 tane 1 birim \t(    *    )  sekillerinden olusan 8 tane gemi vardir \n'
       'tablodaki dogru kordinatlari girerek 15 tahminde bulmaya calisiniz...Basarilar')
@@ -86,11 +87,19 @@ while gemi_sayisi<=8:
         y = r.randint(1, 10)
         gemi8 = [[y, x]]
         gemiler.append(gemi8)
-
+gemi1_hamleler=[]
+gemi2_hamleler=[]
+gemi3_hamleler=[]
+gemi4_hamleler=[]
+gemi5_hamleler=[]
+gemi6_hamleler=[]
+gemi7_hamleler=[]
+gemi8_hamleler=[]
+#yapilan hamleleri toplamak icin bos yapilan hamleler listesi olusturduk
 yapilan_hamleler = []
-dogru_hamleler=[]
-hak=1
-while hak<=15:
+dogru_gemiler=[]
+hak=15
+while hak>0:
     try:        # try-except ile sayidan farkli deger girilmesini kontrol ettik
         print(f'{hak} . hakkiniz....basarilar') #kac hakki kaldigini bildirdik
         # kullanicidan input ile kordinatlari aldik
@@ -107,84 +116,95 @@ while hak<=15:
             else:
                 yapilan_hamleler.append(hamle)  # yapilan hamleleri listemizde ekledik
                 tahta[x][y] = 'X'  # 'X' isareti ile oyun tahtamizda yapilan hamleleri belirttik
-                # if'ler ile gemileri bulma durumlarini inceledik
+                # if'ler ile gemilere isabet etme durumlarini inceledik
                 if hamle in gemi1:
-                    # tahta uzerindeki gemilerin indexlerine isabet ettigini '*' isareti kullanarak belirledik
-                    tahta[gemi1[0][0]][gemi1[0][1]] = '*'
-                    tahta[gemi1[1][0]][gemi1[1][1]] = '*'
-                    tahta[gemi1[2][0]][gemi1[2][1]] = '*'
-                    tahta[gemi1[3][0]][gemi1[3][1]] = '*'
-                    print('...BOOOOOOOOM...Tebrikler...1.gemiyi buldunuz.......<<<<<<<\n')
-                    dogru_hamleler.append(hamle)  # dogru hamleleri listemize ekledik oyunu kazanma durumunu bulmak icin
-                    for i in gemi1:  # dogru hamle yapildiginda geminin bulundugu noktalari yapilan ham. list. ekledik
-                        yapilan_hamleler.append(i)
+                    hak += 1
+                    # gemilere isabet ettigini '*' isareti kullanarak belirledik
+                    tahta[x][y] = '*'
+                    yapilan_hamleler.append(hamle)
+                    gemi1_hamleler.append(hamle)
+                    if len(gemi1_hamleler) == 4:
+                        print('>>>>>>....Tebrikler...( * * * * ) 4 birimlik 1.gemiyi buldunuz.......<<<<<<<\n')
+                        # gemiyi tamamen bulunca dogru_gemiler list. ekledik oyunu kazanma durumunu bulmak icin
+                        dogru_gemiler.append(gemi1)
                 if hamle in gemi2:
-                    # tahta uzerindeki gemilerin indexlerine isabet ettigini '*' isareti kullanarak belirledik
-                    tahta[gemi2[0][0]][gemi2[0][1]] = '*'
-                    tahta[gemi2[1][0]][gemi2[1][1]] = '*'
-                    tahta[gemi2[2][0]][gemi2[2][1]] = '*'
-                    tahta[gemi2[3][0]][gemi2[3][1]] = '*'
-                    for i in gemi2:  # dogru hamle yapildiginda geminin bulundugu noktalari yapilan ham. list. ekledik
-                        yapilan_hamleler.append(i)
-                    print('...BOOOOOOOOM...Tebrikler...2.gemiyi buldunuz.......<<<<<<<\n')
-                    dogru_hamleler.append(hamle)  #dogru hamleleri listemize ekledik oyunu kazanma durumunu bulmak icin
+                    hak += 1
+                    # gemilere isabet ettigini '*' isareti kullanarak belirledik
+                    tahta[x][y] = '*'
+                    yapilan_hamleler.append(hamle)
+                    gemi2_hamleler.append(hamle)
+                    if len(gemi2_hamleler) == 4:
+                        print('>>>>>>....Tebrikler...( * * * * ) 4 birimlik 2.gemiyi buldunuz.......<<<<<<<\n')
+                        # gemiyi tamamen bulunca dogru_gemiler list. ekledik oyunu kazanma durumunu bulmak icin
+                        dogru_gemiler.append(gemi2)
                 if hamle in gemi3:
-                    # tahta uzerindeki gemilerin indexlerine isabet ettigini '*' isareti kullanarak belirledik
-                    tahta[gemi3[0][0]][gemi3[0][1]] = '*'
-                    tahta[gemi3[1][0]][gemi3[1][1]] = '*'
-                    tahta[gemi3[2][0]][gemi3[2][1]] = '*'
-                    for i in gemi3:  #dogru hamle yapildiginda geminin bulundugu noktalari yapilan ham. list. ekledik
-                        yapilan_hamleler.append(i)
-                    print('...BOOOOOOOOM...Tebrikler...3.gemiyi buldunuz.......<<<<<<<\n')
-                    dogru_hamleler.append(hamle)  #dogru hamleleri listemize ekledik oyunu kazanma durumunu bulmak icin
+                    hak += 1
+                    # gemilere isabet ettigini '*' isareti kullanarak belirledik
+                    tahta[x][y] = '*'
+                    yapilan_hamleler.append(hamle)
+                    gemi3_hamleler.append(hamle)
+                    if len(gemi3_hamleler) == 3:
+                        print('>>>>>>....Tebrikler...( * * * ) 3 birimlik 1.gemiyi buldunuz.......<<<<<<<\n')
+                        # dogru hamleleri listemize ekledik oyunu kazanma durumunu bulmak icin
+                        dogru_gemiler.append(gemi3)
                 if hamle in gemi4:
-                    # tahta uzerindeki gemilerin indexlerine isabet ettigini '*' isareti kullanarak belirledik
-                    tahta[gemi4[0][0]][gemi4[0][1]] = '*'
-                    tahta[gemi4[1][0]][gemi4[1][1]] = '*'
-                    tahta[gemi4[2][0]][gemi4[2][1]] = '*'
-                    for i in gemi4:  # dogru hamle yapildiginda geminin bulundugu noktalari yapilan ham. list. ekledik
-                        yapilan_hamleler.append(i)
-                    print('...BOOOOOOOOM...Tebrikler...4.gemiyi buldunuz.......<<<<<<<\n')
-                    dogru_hamleler.append(hamle)  #dogru hamleleri listemize ekledik oyunu kazanma durumunu bulmak icin
+                    hak += 1
+                    # gemilere isabet ettigini '*' isareti kullanarak belirledik
+                    tahta[x][y] = '*'
+                    yapilan_hamleler.append(hamle)
+                    gemi4_hamleler.append(hamle)
+                    if len(gemi4_hamleler) == 3:
+                        print('>>>>>>....Tebrikler...( * * * ) 3 birimlik 2.gemiyi buldunuz.......<<<<<<<\n')
+                        # dogru hamleleri listemize ekledik oyunu kazanma durumunu bulmak icin
+                        dogru_gemiler.append(gemi4)
                 if hamle in gemi5:
-                    # tahta uzerindeki gemilerin indexlerine isabet ettigini '*' isareti kullanarak belirledik
-                    tahta[gemi5[0][0]][gemi5[0][1]] = '*'
-                    tahta[gemi5[1][0]][gemi5[1][1]] = '*'
-                    for i in gemi5:  # dogru hamle yapildiginda geminin bulundugu noktalari yapilan ham. list. ekledik
-                        yapilan_hamleler.append(i)
-                    print('...BOOOOOOOOM...Tebrikler...5.gemiyi buldunuz.......<<<<<<<\n')
-                    dogru_hamleler.append(
-                        hamle)  # dogru hamleleri listemize ekledik oyunu kazanma durumunu bulmak icin
+                    hak += 1
+                    # gemilere isabet ettigini '*' isareti kullanarak belirledik
+                    tahta[x][y] = '*'
+                    yapilan_hamleler.append(hamle)
+                    gemi5_hamleler.append(hamle)
+                    if len(gemi5_hamleler) == 2:
+                        print('>>>>>>....Tebrikler...( * * ) 2 birimlik 1.gemiyi buldunuz.......<<<<<<<\n')
+                        # dogru hamleleri listemize ekledik oyunu kazanma durumunu bulmak icin
+                        dogru_gemiler.append(gemi5)
                 if hamle in gemi6:
-                    # tahta uzerindeki gemilerin indexlerine isabet ettigini '*' isareti kullanarak belirledik
-                    tahta[gemi6[0][0]][gemi6[0][1]] = '*'
-                    tahta[gemi6[1][0]][gemi6[1][1]] = '*'
-                    for i in gemi6:  # dogru hamle yapildiginda geminin bulundugu noktalari yapilan ham. list. ekledik
-                        yapilan_hamleler.append(i)
-                    print('...BOOOOOOOOM...Tebrikler...6.gemiyi buldunuz.......<<<<<<<\n')
-                    dogru_hamleler.append(hamle)  #dogru hamleleri listemize ekledik oyunu kazanma durumunu bulmak icin
+                    hak += 1
+                    # gemilere isabet ettigini '*' isareti kullanarak belirledik
+                    tahta[x][y] = '*'
+                    yapilan_hamleler.append(hamle)
+                    gemi6_hamleler.append(hamle)
+                    if len(gemi6_hamleler) == 2:
+                        print('>>>>>>....Tebrikler...( * * ) 2 birimlik 2.gemiyi buldunuz.......<<<<<<<\n')
+                        # dogru hamleleri listemize ekledik oyunu kazanma durumunu bulmak icin
+                        dogru_gemiler.append(gemi6)
                 if hamle in gemi7:
-                    # tahta uzerindeki gemilerin indexlerine isabet ettigini '*' isareti kullanarak belirledik
-                    tahta[gemi7[0][0]][gemi7[0][1]] = '*'
-                    for i in gemi7:  # dogru hamle yapildiginda geminin bulundugu noktalari yapilan ham. list. ekledik
-                        yapilan_hamleler.append(i)
-                    print('...BOOOOOOOOM...Tebrikler...7.gemiyi buldunuz.......<<<<<<<\n')
-                    dogru_hamleler.append(hamle)  #dogru hamleleri listemize ekledik oyunu kazanma durumunu bulmak icin
+                    hak += 1
+                    # gemilere isabet ettigini '*' isareti kullanarak belirledik
+                    tahta[x][y] = '*'
+                    yapilan_hamleler.append(hamle)
+                    gemi7_hamleler.append(hamle)
+                    if len(gemi7_hamleler) == 1:
+                        print('>>>>>>....Tebrikler...( * ) 1 birimlik 1.gemiyi buldunuz.......<<<<<<<\n')
+                        # dogru hamleleri listemize ekledik oyunu kazanma durumunu bulmak icin
+                        dogru_gemiler.append(gemi7)
                 if hamle in gemi8:
-                    # tahta uzerindeki gemilerin indexlerine isabet ettigini '*' isareti kullanarak belirledik
-                    tahta[gemi8[0][0]][gemi8[0][1]] = '*'
-                    for i in gemi8:  # dogru hamle yapildiginda geminin bulundugu noktalari yapilan ham. list. ekledik
-                        yapilan_hamleler.append(i)
-                    print('...BOOOOOOOOM...Tebrikler...8.gemiyi buldunuz.......<<<<<<<\n')
-                    dogru_hamleler.append(hamle)  #dogru hamleleri listemize ekledik oyunu kazanma durumunu bulmak icin
+                    hak += 1
+                    # gemilere isabet ettigini '*' isareti kullanarak belirledik
+                    tahta[x][y] = '*'
+                    yapilan_hamleler.append(hamle)
+                    gemi8_hamleler.append(hamle)
+                    if len(gemi8_hamleler) == 1:
+                        print('>>>>>>....Tebrikler...( * ) 1 birimlik 2.gemiyi buldunuz.......<<<<<<<\n')
+                        # dogru hamleleri listemize ekledik oyunu kazanma durumunu bulmak icin
+                        dogru_gemiler.append(gemi8)
                 tablo()     # fonksiyon ile oyun tahtamizi ekrana yazdirdik
-                hak += 1    # hakkimizi 1 arttirdik
+                hak -= 1    # hakkimizi 1 azalttik
         else:
             print('\n!!!...UYARI..."1,2,3,4,5,6,7,8,9,10" arasindan bir sayi giriniz\n')
             continue
-        if len(dogru_hamleler) == 8:  # dogru hamleler sayisi ile kazanma durumunu kontrol ettik
+        if len(dogru_gemiler) == 8:  # dogru hamleler sayisi ile kazanma durumunu kontrol ettik
             print('\nTEBRIKLER OYUNU KAZANDINIZ')
             quit()
     except ValueError:
         print('Lutfen "1,2,3,4,5,6,7,8,9,10" arasindan bir sayi giriniz')
-print(f'{hak-1} hakkiniz bitti.....KAYBETTINIZ')
+print(f'{hak} hak....hakkiniz bitti.....KAYBETTINIZ')
