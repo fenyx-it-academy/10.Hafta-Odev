03bd73833b641b9d74998b898ce3c910ea52f0a2
HTunctepe <hsn.tunctepe@gmail.com>
2019-08-24 14:32:04 +0200

    Gec gonderi icin kusura bakmayin. Hem vakit problemim oldu, hem de oyunu en zonr sekilde yaptim. Gemiler random bir sekilde hem yatay, hem dikey uretilebiliyor ve birbiriyle cakismiyor.

--- 10.Hafta-Odev-HTunctepe.py
+++ 10.Hafta-Odev-HTunctepe.py
@@ -1,21 +1,3 @@
-# Deniz olarak varsayacagimiz 10x10'luk bir tablo olusturun (X-O-X oyununa benzer).
-# 2 adet 4 birimlik,
-# 2 adet 3 birimlik,
-# 2 adet 2 birimlik ve
-# 2 adet 1 birimlik gemiler yerlestirin.
-
-# Yerlestirdiginiz gemileri kullanici gormeyecek. Kullanicidan tablo uzerindeki herhangi bir noktaya hamlede
-# bulunmasini isteyin. Kullanicinin hamlesi gemilerin herhangi bir noktasina isabet ederse kullaniciya tablo
-# uzerinde bunu gosterin. Ayni sekilde bosa atis yaptiginda da kullaniciya tablo uzerinde bunu gosterin ve
-# kullanicinin 5 sn boyunca hamle yapmasini engelleyin. Kullanici daha once hamle yapmis oldugu bir noktaya
-# tekrar hamlede bulunursa bunu engelleyin. Kullaniciya toplamda 15 yanlis hamle hakki verin. Kullanici
-# tum gemileri vurdugunda oyunu bitirin.
-#
-# Oyunu mumkun oldugunca fonksiyonlar kullanarak yapmanizi istiyoruz.
-#
-# BONUS(ISTEGE BAGLI): Yukaridaki versiyonda gemiler sabit. Oyunu her actigimizda gemilerin yeri degismiyor.
-# Isteyenler gemilerin random yerlestirildigi bir versiyonunu yapabilir.
-
 import random
 import time
 
@@ -65,7 +47,7 @@ def gameBoardPrinter(board):
         print('\t'.expandtabs(30), str(i), ' ', ' '.join(r))
         print('\n')
         i += 1
-        
+
 
 
 def positionCheck(shipPositionList, r, c):
